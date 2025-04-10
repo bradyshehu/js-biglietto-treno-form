447fa56744e06a57a40952a8b72425064819b5e1
bradyshehu <shehu.brady@gmail.com>
2025-04-10 18:52:14 +0200

    finish js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ const distanceInput = document.getElementById("distance");
 const ageInput = document.getElementById("age-range");
 const createButton = document.getElementById("generate");
 const cancelButton = document.getElementById("cancel");
+const wrongForm = document.getElementById("wrong-form");
+const ticket = document.getElementById("ticket");
+const ticketName = document.getElementById("ticket-name");
+const ticketType = document.getElementById("ticket-type");
+const ticketPrice = document.getElementById("ticket-price");
 
 createButton.addEventListener("click", function (event) {
   event.preventDefault();
 
   const name = nameInput.value;
-  const distance = parseInt(distanceInput.value);
+  const distance = distanceInput.value;
   const age = ageInput.value;
   console.log(name);
   console.log(distance);
@@ -21,23 +26,34 @@ createButton.addEventListener("click", function (event) {
   }
 
   function finalPrice(distance, age) {
+    ticketName.innerText = `${name}`;
     const price = distance * 0.21;
 
     if (age === "minor") {
       console.log("minorenne");
       const prezzo = applyDiscount(price, 20);
       console.log(prezzo);
+      ticketType.innerText = "Biglietto minorenni";
+      ticketPrice.innerText = `${prezzo}`;
     } else if (age === "over-65") {
       console.log("over-65");
       const prezzo = applyDiscount(price, 40);
       console.log(prezzo);
+      ticketType.innerText = "Biglietto over-65";
+      ticketPrice.innerText = `${prezzo}`;
     } else {
       console.log("adulto");
       const prezzo = price;
       console.log(prezzo);
+      ticketType.innerText = "Biglietto standard";
+      ticketPrice.innerText = `${prezzo}`;
     }
+    wrongForm.classList.add("d-none");
+    ticket.classList.remove("d-none");
   }
-  !isNaN(distance) ? finalPrice(distance, age) : ;
+  !isNaN(distance)
+    ? finalPrice(distance, age)
+    : wrongForm.classList.remove("d-none");
 });
 
 cancelButton.addEventListener("click", function () {
